ba58fe539a2e3291f950e44c021d8770e58f73b3
anarysumbetov <74860324+anarysumbetov@users.noreply.github.com>
2022-05-30 14:04:41 +0600

    Attacking pawn is complete

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -2,12 +2,25 @@ import { PieceType, TeamType, Piece } from "./../components/Chessboard/Chessboar
 
 export default class Referee {
     tileIsOccupied(x: number, y: number, boardState: Piece[]): boolean {
-        console.log("Checking if tile is occupied...")
-
         const piece = boardState.find(p => p.x === x && p.y === y)
-
+        
         if (piece) {
             return true;
+        } else {
+            return false;
+        }
+    }
+
+    TileIsOccupiedByOpponent(
+        x: number, 
+        y: number, 
+        boardState: Piece[], 
+        team: TeamType
+        ): boolean {
+        const piece = boardState.find((p) => p.x === x && p.y === y && p.team !== team);
+
+        if(piece) {
+        return true;
         } else {
             return false;
         }
@@ -22,16 +35,17 @@ export default class Referee {
         team: TeamType,
         boardState: Piece[]
         ) {
-        console.log("Referee is checking the move...");
-        console.log(`Previous location: (${px},${py})`);
-        console.log(`Current location: (${x},${y})`);
-        console.log(`Piece type: ${type}`);
-        console.log(`Team: ${team}`);
+        // console.log("Referee is checking the move...");
+        // console.log(`Previous location: (${px},${py})`);
+        // console.log(`Current location: (${x},${y})`);
+        // console.log(`Piece type: ${type}`);
+        // console.log(`Team: ${team}`);
 
         if (type === PieceType.PAWN) {
             const specialRow = (team === TeamType.OUR) ? 1 : 6;
             const pawnDirection = (team === TeamType.OUR) ? 1 : -1;
 
+            //MOVEMENT LOGIC
             if (px === x && py === specialRow && y - py === 2*pawnDirection) {
                 if (
                     !this.tileIsOccupied(x, y, boardState) && 
@@ -43,6 +57,20 @@ export default class Referee {
                 if (!this.tileIsOccupied(x, y, boardState)) {
                     return true;
                 }
+            }
+            //ATTACK LOGIC
+            else if (x - px === -1 && y - py === pawnDirection) {
+                //ATTACK IN UPPER OR BOTTOM LEFT CORNER
+                console.log("upper / bottom left");
+                if(this.TileIsOccupiedByOpponent(x, y, boardState, team)) {
+                    return true;
+                }
+            } else if (x - px === 1 &&  y - py === pawnDirection) {
+                //ATTACK IN THE UPPER OR BOTTOM RIGHT CORNER
+                console.log("upper / bottom right");
+                if(this.TileIsOccupiedByOpponent(x, y, boardState, team)) {
+                    return true;
+                }  
             }
         }
 
