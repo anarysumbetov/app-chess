7560afb8e2babfcce53037365681d6b7b0558950
anarysumbetov <74860324+anarysumbetov@users.noreply.github.com>
2022-06-10 15:36:59 +0600

    Refactoring referee is complete

---
+++ src/referee/rules/KnightRules.ts
@@ -0,0 +1,27 @@
+import { Position, TeamType, Piece } from "../../Constants.ts";
+import { tileIsEmptyOrOccupiedByOpponent } from "./GeneralRules.ts";
+
+export const knightMove = (initialPosition: Position, desiredPosition: Position, team: TeamType, boardState: Piece[]): boolean => {
+    for (let i = -1; i < 2; i+=2) {
+        for (let j = -1; j < 2; j+=2) {
+            //TOP AND BOTTOM SIDE MOVEMENT
+            if (desiredPosition.y - initialPosition.y === 2 * i) {
+                if (desiredPosition.x - initialPosition.x === j) {
+                    if (tileIsEmptyOrOccupiedByOpponent(desiredPosition, boardState, team)) {
+                        return true;
+                    }
+                }
+            }
+
+            //RIGHT AND LEFT SIDE MOVEMENT
+            if (desiredPosition.x - initialPosition.x === 2 * i) {
+                if (desiredPosition.y - initialPosition.y === j) {
+                    if (tileIsEmptyOrOccupiedByOpponent(desiredPosition, boardState, team)) {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
