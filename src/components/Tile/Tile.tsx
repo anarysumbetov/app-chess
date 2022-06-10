a4d0e3d74c536e7e7946c1aa83d94c3b44759968
anarysumbetov <74860324+anarysumbetov@users.noreply.github.com>
2022-06-11 00:01:20 +0600

    Pawn Promotion is complete, the last episod for this moment

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -5,7 +5,7 @@ interface Props {
     number: number;
 }
 
-export default function Tile({number, image }: Props) {
+export default function Tile({ number, image }: Props) {
     if(number % 2 === 0) {
         return <div className="tile black-tile">
             {image && <div style={{backgroundImage: `url(${image})`}} className="chess-piece"></div>}
